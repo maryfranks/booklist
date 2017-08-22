ef6378a57b41584cb313e5125d39ab7561e55d74
maryfranks <maryfranks@gmail.com>
2017-08-22 16:53:42 -0400

    add view for html and text to books controller index method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,16 @@
 class BooksController < ApplicationController
 
+  def index
+
+    @books = Book.all 
+
+    respond_to do | format |
+
+      format.html
+      format.text
+
+    end
+
+  end
 
 end
